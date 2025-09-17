39f80df126ccaaa27ad013456c6e6239969d2d18
myron302 <myron302@gmail.com>
2025-09-16 22:53:41 -0400

    feat: add mobile navigation

---
+++ client/src/components/MobileNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MobileNav = () => (
+  <nav className="mobile-nav">
+    {/* Add mobile nav links here */}
+    <a href="/">Home</a>
+    <a href="/recipes">Recipes</a>
+    <a href="/about">About</a>
+  </nav>
+);
+
+export default MobileNav;
