d6f2c350f427ae74d20db9ab27f1f8d86085a77e
myron302 <myron302@gmail.com>
2025-09-17 09:38:12 -0400

    Create initial App component for Chefsire

---
+++ client/src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Chefsire - Coming Soon</h1>
+      <p>Recipe app rebuilding...</p>
+    </div>
+  )
+}
+
+export default App
