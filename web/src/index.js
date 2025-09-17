6483e133b25f912aa19bb496fb9c7191a1955b4c
myron302 <myron302@gmail.com>
2025-09-16 20:41:20 -0400

    Add missing src/index.js for React app build

---
+++ web/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>Welcome to Chefsire Web!</h1>
+      <p>Your React app is running successfully.</p>
+    </div>
+  </React.StrictMode>
+);
